d94401437dc722416450313ec256dc7bc874e0a4
Munachiso Ilokah <munachisoilokah@gmail.com>
2022-01-17 15:44:59 -0500

    Modified presentation of info on admin page and added other functionality.

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,8 +3,38 @@ from .models import Inventory
 from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
+# class InventoryInline(admin.TabularInline):
+#     model = Inventory
+#     extra = 0
+
+
+# class InventoryAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     (None,                  {'fields': ['title']}),
+    #     ('Merchant',            {'fields': ['merchant']}),
+    #     ('Description',         {'fields': ['description']}),
+    #     ('Specifications',      {'fields': ['specifications']}),
+    #     ('Date Info',           {'fields': ['date']}),
+    #     ('Manufacturer',        {'fields': ['manufacturer']}),
+    #     ('Quantity',            {'fields': ['quantity']}),
+    # ]
+    # # inlines = [InventoryInline,]
+    # list_display = ('title', 'merchant', 'description', 'specifications', 'manufacturer', 'quantity', 'date',)
+    # list_filter = ['merchant']
+    # search_fields = ['title']
+
 @admin.register(Inventory)
 class InventoryAdmin(ImportExportModelAdmin):
-    pass
-
-# admin.site.register(Inventory)
+    fieldsets = [
+        (None,                  {'fields': ['title']}),
+        ('Merchant',            {'fields': ['merchant']}),
+        ('Description',         {'fields': ['description']}),
+        ('Specifications',      {'fields': ['specifications']}),
+        ('Date Info',           {'fields': ['date']}),
+        ('Manufacturer',        {'fields': ['manufacturer']}),
+        ('Quantity',            {'fields': ['quantity']}),
+    ]
+    # inlines = [InventoryInline,]
+    list_display = ('title', 'merchant', 'description', 'specifications', 'manufacturer', 'quantity', 'date',)
+    list_filter = ['merchant']
+    search_fields = ['title']
